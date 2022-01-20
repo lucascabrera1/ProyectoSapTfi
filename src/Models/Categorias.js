1d06c276e9dfc260a40d1c4938a93421b9a08d8f
lucas_cabrera1 <lucas_cabrera1@hotmail.com>
2022-01-19 21:06:39 -0300

    agrega marcas

--- src/Models/Categorias.js
+++ src/Models/Categorias.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const CategoriasSchema = new Schema({
+    _id:{type: String, required: true},
     nombre:{type: String, required: true}
 })
 
